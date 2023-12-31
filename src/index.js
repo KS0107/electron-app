0d3f00a1d19d8f8fbbe24dc0db3df936e3b07f3a
Kushagra Srivastava <kushagra.srivastava@student.manchester.ac.uk>
2023-12-31 20:37:24 +0100

    UI edits

--- src/index.js
+++ src/index.js
@@ -1,49 +1,170 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
+let mainWindow;
+let db;
+
+function createDatabase() {
+  db = new sqlite3.Database('jobDB.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Connected to the SQLite database.');
+      createTable(insertRandomJobs); // Create table and then insert jobs
+    }
+  });
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+function createTable(callback) {
+  db.run(`CREATE TABLE IF NOT EXISTS jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    company TEXT,
+    location TEXT,
+    description TEXT,
+    status TEXT,
+    created_at TEXT
+  )`, (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Table created');
+    callback(); // Call the callback function to insert jobs after table creation
+  });
+}
+
+function insertRandomJobs() {
+  for (let i = 0; i < 10; i++) {
+    const job = getRandomJob();
+    const sql = `INSERT INTO jobs (title, company, location, description, status, created_at) VALUES (?, ?, ?, ?, ?, ?)`;
+    db.run(sql, [job.title, job.company, job.location, job.description, job.status, job.created_at], function(err) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+      }
+    });
+  }
+}
+
+function insertJob(title, company, location, description, status, created_at) {
+  const sql = `INSERT INTO jobs (title, company, location, description, status, created_at) VALUES (?, ?, ?, ?, ?, ?)`;
+  db.run(sql, [title, company, location, description, status, created_at], function(err) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      getAllJobs(); // Optionally refresh the job list after adding a new job
+    }
+  });
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // mainWindow.webContents.openDevTools();
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  // IPC listener for the renderer's request to load jobs
+  ipcMain.on('request-jobs', (event) => {
+    getAllJobs();
+  });
+  ipcMain.on('delete-jobs', (event) => {
+    clearDatabase();
+  });
+  ipcMain.on('add-job', (event, job) => {
+    insertJob(job.title, job.company, job.location, job.description, job.status, job.created_at);
+  });
+}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createDatabase();
+  createWindow();
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  // if (process.platform !== 'darwin') {
+  //   db.close((err) => {
+  //     if (err) {
+  //       console.error(err.message);
+  //     }
+  //     console.log('Closed the database connection.');
+  //   });
+  //   app.quit();
+  // }
+  app.quit();
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+function getAllJobs() {
+  const sql = `SELECT * FROM jobs`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    mainWindow.webContents.send('jobs-data', rows);
+  });
+}
+
+function getRandomJob() {
+  const titles = ['Software Developer', 'Project Manager', 'Graphic Designer', 'Data Analyst'];
+  const companies = ['Tech Corp', 'Design Studio', 'Data Inc.', 'Innovate LLC'];
+  const locations = ['New York', 'San Francisco', 'Austin', 'Seattle'];
+  const descriptions = ['Lorem ipsum dolor sit amet', 'Consectetur adipiscing elit', 'Sed do eiusmod tempor incididunt', 'Ut labore et dolore magna aliqua'];
+  const statuses = ['Open', 'Closed', 'Pending'];
+
+  return {
+    title: titles[Math.floor(Math.random() * titles.length)],
+    company: companies[Math.floor(Math.random() * companies.length)],
+    location: locations[Math.floor(Math.random() * locations.length)],
+    description: descriptions[Math.floor(Math.random() * descriptions.length)],
+    status: statuses[Math.floor(Math.random() * statuses.length)],
+    created_at: new Date().toISOString()
+  };
+}
+
+function clearDatabase() {
+  const sql = `DELETE FROM jobs`;
+  db.run(sql, [], (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Database cleared');
+  });
+}
+
+
+
+ipcMain.on('update-job', (event, job) => {
+  updateJob(job);
+});
+
+function updateJob(job) {
+  const sql = `UPDATE jobs SET title = ?, company = ?, location = ?, description = ?, status = ?, created_at = ? WHERE id = ?`;
+  db.run(sql, [job.title, job.company, job.location, job.description, job.status, job.created_at, job.id], function(err) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`Job updated with rowid ${job.id}`);
+      getAllJobs(); // Refresh the job list
+    }
+  });
+}
+
---
+++ src/renderer.js
@@ -0,0 +1,77 @@
+const { ipcRenderer } = require('electron');
+
+function loadJobs() {
+  ipcRenderer.send('request-jobs');
+}
+
+function clearJobs() {
+  ipcRenderer.send('delete-jobs');
+}
+
+ipcRenderer.on('jobs-data', (event, jobs) => {
+  const tableContainer = document.getElementById('jobs-table-container');
+  tableContainer.innerHTML = ''; // Clear existing content
+
+  const table = document.createElement('table');
+  table.style.width = '100%';
+  table.createTHead().innerHTML = '<tr><th>Title</th><th>Company</th><th>Location</th><th>Description</th><th>Status</th></tr>';
+
+  const tbody = table.createTBody();
+  jobs.forEach(job => {
+    const row = tbody.insertRow();
+    row.innerHTML = `<td>${job.title}</td><td>${job.company}</td><td>${job.location}</td><td>${job.description}</td><td>${job.status}</td>`;
+    row.addEventListener('click', () => editJob(job, row)); // Pass the row as well
+  });
+
+  tableContainer.appendChild(table);
+});
+
+document.getElementById('jobForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const jobId = document.getElementById('jobId').value;
+  const job = {
+    id: jobId,
+    title: document.getElementById('title').value,
+    company: document.getElementById('company').value,
+    location: document.getElementById('location').value,
+    description: document.getElementById('description').value,
+    status: document.getElementById('status').value,
+    created_at: new Date().toISOString()
+  };
+
+  if (jobId) {
+    ipcRenderer.send('update-job', job);
+  } else {
+    ipcRenderer.send('add-job', job);
+  }
+});
+
+function editJob(job, rowElement) {
+  document.getElementById('jobId').value = job.id;
+  document.getElementById('title').value = job.title;
+  document.getElementById('company').value = job.company;
+  document.getElementById('location').value = job.location;
+  document.getElementById('description').value = job.description;
+  document.getElementById('status').value = job.status;
+  document.getElementById('jobFormButton').textContent = 'Edit Job';
+
+  // Highlight the selected row
+  const previouslySelected = document.querySelector('.highlighted');
+  if (previouslySelected) {
+    previouslySelected.classList.remove('highlighted');
+  }
+  rowElement.classList.add('highlighted');
+
+  document.getElementById('jobForm').scrollIntoView();
+}
+
+ipcRenderer.on('jobs-data', (event, jobs) => {
+  // Existing code to populate the jobs table
+  resetForm();
+});
+
+function resetForm() {
+  document.getElementById('jobForm').reset();
+  document.getElementById('jobId').value = '';
+  document.getElementById('jobFormButton').textContent = 'Add Job';
+}
